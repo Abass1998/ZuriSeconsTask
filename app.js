bf3338d238bfda9c7c17ff3e8935de387d2be617
Oluokun Abass Odunola <abassodunola56@gmail.com>
2021-04-07 18:10:50 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const hamburgers = document.getElementById("hamburger");
+const navUl = document.getElementById("nav-lu");
+hamburgers.addEventListener("click", ()=>{
+      navUl.classList.toggle("show");
+});
+
+function myFunction(x) {
+  x.classList.toggle("change");
+}function myFunction(x) {
+  x.classList.toggle("change");
+}
